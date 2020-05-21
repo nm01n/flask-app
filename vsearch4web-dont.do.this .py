ff203518bfe27417b6b1c7d60d19ebbde82606dc
nm01n <nemo0mb@gmail.com>
2020-05-21 15:22:12 +0300

    files fixed

--- vsearch4web-dont.do.this .py
+++ vsearch4web-dont.do.this .py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, request, escape, session
-from flask import copy_current_request_context
 from vsearch import search4letters
+
+from DBcm import UseDatabase
 from checker import check_logged_in
 
-from threading import Thread
+import mysql.connector
+
 from time import sleep
 
 app = Flask(__name__)
@@ -26,32 +28,32 @@ def do_logout() -> str:
     return 'You are now logged out.'
 
 
+def log_request(req: 'flask_request', res: str) -> None:
+    """Log details of the web request and the results."""
+
+    # raise Exception("Something awful just happened.")
+
+    with UseDatabase(app.config['dbconfig']) as cursor:
+        _SQL = """insert into log
+                  (phrase, letters, ip, browser_string, results)
+                  values
+                  (%s, %s, %s, %s, %s)"""
+        cursor.execute(_SQL, (req.form['phrase'],
+                              req.form['letters'],
+                              req.remote_addr,
+                              req.user_agent.browser,
+                              res, ))
+
+
 @app.route('/search4', methods=['POST'])
 def do_search() -> 'html':
     """Extract the posted data; perform the search; return results."""
-
-    @copy_current_request_context
-    def log_request(req: 'flask_request', res: str) -> None:
-        """Log details of the web request and the results."""
-        sleep(15)  # This makes log_request really slow...
-        with UseDatabase(app.config['dbconfig']) as cursor:
-            _SQL = """insert into log
-                    (phrase, letters, ip, browser_string, results)
-                    values
-                    (%s, %s, %s, %s, %s)"""
-            cursor.execute(_SQL, (req.form['phrase'],
-                                req.form['letters'],
-                                req.remote_addr,
-                                req.user_agent.browser,
-                                res, ))
-
     phrase = request.form['phrase']
     letters = request.form['letters']
     title = 'Here are your results:'
     results = str(search4letters(phrase, letters))
     try:
-        t = Thread(target=log_request, args=(request, results))
-        t.start()
+        log_request(request, results)
     except Exception as err:
         print('***** Logging failed with this error:', str(err))
     return render_template('results.html',
@@ -79,20 +81,18 @@ def view_the_log() -> 'html':
                     from log"""
             cursor.execute(_SQL)
             contents = cursor.fetchall()
-        # raise Exception("Some unknown exception.")
         titles = ('Phrase', 'Letters', 'Remote_addr', 'User_agent', 'Results')
         return render_template('viewlog.html',
-                               the_title='View Log',
-                               the_row_titles=titles,
-                               the_data=contents,)
-    except ConnectionError as err:
+                            the_title='View Log',
+                            the_row_titles=titles,
+                            the_data=contents,)
+
+    ### Don't do this as it tightly couples your code to MySQL. ###
+    except mysql.connector.errors.InterfaceError as err:
         print('Is your database switched on? Error:', str(err))
-    except CredentialsError as err:
-        print('User-id/Password issues. Error:', str(err))
-    except SQLError as err:
-        print('Is your query correct? Error:', str(err))
     except Exception as err:
         print('Something went wrong:', str(err))    
+        raise err
     return 'Error'
 
 app.secret_key = 'YouWillNeverGuessMySecretKey'
--- vsearch4web.slow.with.sleep.py
+++ vsearch4web.slow.with.sleep.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template, request, escape, session
-from flask import copy_current_request_context
 from vsearch import search4letters
+
+from DBcm import UseDatabase, ConnectionError, CredentialsError, SQLError
 from checker import check_logged_in
 
-from threading import Thread
 from time import sleep
 
 app = Flask(__name__)
@@ -26,32 +26,34 @@ def do_logout() -> str:
     return 'You are now logged out.'
 
 
+def log_request(req: 'flask_request', res: str) -> None:
+    """Log details of the web request and the results."""
+
+    # raise Exception("Something awful just happened.")
+    
+    sleep(15)  # This makes log_request really slow...
+
+    with UseDatabase(app.config['dbconfig']) as cursor:
+        _SQL = """insert into log
+                  (phrase, letters, ip, browser_string, results)
+                  values
+                  (%s, %s, %s, %s, %s)"""
+        cursor.execute(_SQL, (req.form['phrase'],
+                              req.form['letters'],
+                              req.remote_addr,
+                              req.user_agent.browser,
+                              res, ))
+
+
 @app.route('/search4', methods=['POST'])
 def do_search() -> 'html':
     """Extract the posted data; perform the search; return results."""
-
-    @copy_current_request_context
-    def log_request(req: 'flask_request', res: str) -> None:
-        """Log details of the web request and the results."""
-        sleep(15)  # This makes log_request really slow...
-        with UseDatabase(app.config['dbconfig']) as cursor:
-            _SQL = """insert into log
-                    (phrase, letters, ip, browser_string, results)
-                    values
-                    (%s, %s, %s, %s, %s)"""
-            cursor.execute(_SQL, (req.form['phrase'],
-                                req.form['letters'],
-                                req.remote_addr,
-                                req.user_agent.browser,
-                                res, ))
-
     phrase = request.form['phrase']
     letters = request.form['letters']
     title = 'Here are your results:'
     results = str(search4letters(phrase, letters))
     try:
-        t = Thread(target=log_request, args=(request, results))
-        t.start()
+        log_request(request, results)
     except Exception as err:
         print('***** Logging failed with this error:', str(err))
     return render_template('results.html',
@@ -82,9 +84,9 @@ def view_the_log() -> 'html':
         # raise Exception("Some unknown exception.")
         titles = ('Phrase', 'Letters', 'Remote_addr', 'User_agent', 'Results')
         return render_template('viewlog.html',
-                               the_title='View Log',
-                               the_row_titles=titles,
-                               the_data=contents,)
+                            the_title='View Log',
+                            the_row_titles=titles,
+                            the_data=contents,)
     except ConnectionError as err:
         print('Is your database switched on? Error:', str(err))
     except CredentialsError as err:
